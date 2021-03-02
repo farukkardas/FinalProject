eaff749092c4e25419fd7a893514fcfb39a18fc2
gafar113 <falgamerss@gmail.com>
2021-03-02 16:47:24 +0300

    IHttpContextAccessor hatası gireldi, login olurken ve product eklerken artık sorun çıkmıyor

--- gercekproject/Business/Concrete/UserManager.cs
+++ gercekproject/Business/Concrete/UserManager.cs
@@ -4,16 +4,18 @@ using System.Text;
 using Business.Abstract;
 using Core.Entities.Concrete;
 using DataAccess.Abstract;
+using Microsoft.AspNetCore.Http;
 
 namespace Business.Concrete
 {
     public class UserManager : IUserService
     {
         IUserDal _userDal;
-
-        public UserManager(IUserDal userDal)
+        IHttpContextAccessor _httpContextAccessor;
+        public UserManager(IUserDal userDal, IHttpContextAccessor httpContextAccessor)
         {
             _userDal = userDal;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public List<OperationClaim> GetClaims(User user)
--- gercekproject/WebAPI/Startup.cs
+++ gercekproject/WebAPI/Startup.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -36,8 +37,8 @@ namespace WebAPI
         {
           
             services.AddControllers();
-         
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -55,6 +56,8 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+
+            ServiceTool.Create(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
